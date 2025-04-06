a6c6726113ade39e31cca56df7c99b6ff543b4a9
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-06 13:46:56 +0200

    screen app bar fix

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -6,9 +6,9 @@ import 'package:meals_app/widgets/meal_item.dart';
 import 'package:meals_app/models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.title, required this.meals});
+  const MealsScreen({super.key, this.title, required this.meals});
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void selectMeal(BuildContext context, Meal meal) {
@@ -54,6 +54,10 @@ class MealsScreen extends StatelessWidget {
       );
     }
 
-    return Scaffold(appBar: AppBar(title: Text(title)), body: content);
+    if (title == null) {
+      return content;
+    }
+
+    return Scaffold(appBar: AppBar(title: Text(title!)), body: content);
   }
 }
